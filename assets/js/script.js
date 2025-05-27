2bb761d0c79ec2e056539ef84654b86809810a69
duaksd <casado.eduarda@aluno.ifsp.edu.br>
2025-05-27 13:41:08 -0300

    sobre + login

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const dropdownToggle = document.querySelectorAll('[data-dropdown-toggle]');
     dropdownToggle.forEach((toggle) => {
-        toggle.addEventListener('click', function() {
+        toggle.addEventListener('click', function () {
             const parent = this.closest('.dropdown-item');
+            console.log('teste');
             parent.classList.toggle('active');
         })
     })
-
+    //Fechar o dropdown ao clicar fora
     document.addEventListener('click', function (e) {
         dropdownToggle.forEach((toggle) => {
             const parent = toggle.closest('.dropdown-item');
@@ -24,16 +25,58 @@ const closeSearch = document.getElementById('closeSearch');
 searchToggle.addEventListener('click', () => {
     searchOverlay.style.display = 'flex';
     setTimeout(() => {
-        searchOverlay.querySelector('search-input').focus();
+        searchOverlay.querySelector('.search-input').focus();
     }, 100);
 });
+
+//fechar o botão
 
 closeSearch.addEventListener('click', () => {
     searchOverlay.style.display = 'none';
 })
 
+//fechar com o ESC
 document.addEventListener('keydown', (e) => {
     if (e.key === 'Escape') {
         searchOverlay.style.display = 'none';
+    }
+})
+
+//Galeria
+const imagens = document.querySelectorAll('.galeria img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+
+imagens.forEach((img) => {
+    img.addEventListener('click', () => {
+        lightboxImg.src = img.src;
+        lightbox.classList.add('show');
+    })    
+})
+
+//esconder o lightbox
+lightbox.addEventListener('click', () => {
+    lightbox.classList.remove('show');
+})
+
+
+// Abrir o Login
+const openLoginBtn = document.getElementById('openLogin')
+const containerLogin = document.querySelector('.container-login')
+
+openLoginBtn.addEventListener('click', () => {
+    containerLogin.classList.add('active')
+})
+
+// Fechar ao clicar fora do formuláro ou pressionar ESC
+containerLogin.addEventListener('click', (e) => {
+    if(e.target === containerLogin) {
+        containerLogin.classList.remove('active')
+    }
+})
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        containerLogin.classList.remove('active')
     }
 })
